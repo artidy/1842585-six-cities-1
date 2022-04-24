74bda365bd7cf090d8ec0e24e10ae5140c206652
Andrey Pechennikov <ap@devandy.me>
2022-04-24 15:58:24 +0300

    Изменил функцию импорта данных из файла, для чтения больших данных

--- src/common/file-reader/tsv-file-reader.ts
+++ src/common/file-reader/tsv-file-reader.ts
@@ -1,85 +1,36 @@
-import {readFileSync} from 'fs';
+import EventEmitter from 'events';
+import {createReadStream} from 'fs';
 
 import {FileReaderInterface} from './file-reader.interface.js';
-import Offer from '../../types/offer.js';
 
-class TsvFileReader implements FileReaderInterface {
-  private rawData = '';
-
-  constructor(public fileName: string) { }
-
-  public read(): void {
-    this.rawData = readFileSync(this.fileName, {encoding: 'utf-8'});
+class TsvFileReader extends EventEmitter implements FileReaderInterface {
+  constructor(public fileName: string) {
+    super();
   }
 
-  public toArray(): Offer[] {
-    if (!this.rawData) {
-      return [];
+  public async read(): Promise<void> {
+    const stream = createReadStream(this.fileName, {
+      highWaterMark: 16384,
+      encoding: 'utf-8',
+    });
+
+    let lineRead = '';
+    let endLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of stream) {
+      lineRead += chunk.toString();
+
+      while ((endLinePosition = lineRead.indexOf('\n')) >= 0) {
+        const completeRow = lineRead.slice(0, endLinePosition + 1);
+        lineRead = lineRead.slice(++endLinePosition);
+        importedRowCount++;
+
+        this.emit('line', completeRow);
+      }
     }
 
-    return this.rawData
-      .split('\n')
-      .filter((row) => row.trim() !== '')
-      .map((line) => line.split('\t'))
-      .map(([
-        cityName,
-        cityLatitude,
-        cityLongitude,
-        cityZoom,
-        previewImage,
-        images,
-        title,
-        isFavorite,
-        isPremium,
-        rating,
-        type,
-        bedrooms,
-        maxAdults,
-        price,
-        goods,
-        hostName,
-        hostType,
-        hostAvatar,
-        description,
-        latitude,
-        longitude,
-        zoom,
-        hostId,
-        id
-      ]) => ({
-        city: {
-          name: cityName,
-          location: {
-            latitude: +cityLatitude,
-            longitude: +cityLongitude,
-            zoom: +cityZoom,
-          },
-        },
-        previewImage,
-        images: images.split(','),
-        title,
-        isFavorite: Boolean(isFavorite),
-        isPremium: Boolean(isPremium),
-        rating: +rating,
-        type,
-        bedrooms: +bedrooms,
-        maxAdults: +maxAdults,
-        price: +price,
-        goods: goods.split(','),
-        host: {
-          name: hostName,
-          avatarUrl: hostAvatar,
-          isPro: Boolean(hostType),
-          id: +hostId,
-        },
-        description,
-        location: {
-          latitude: +latitude,
-          longitude: +longitude,
-          zoom: +zoom,
-        },
-        id: +id,
-      }));
+    this.emit('end', importedRowCount);
   }
 }
 
